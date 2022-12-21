e01e0893e2d0cb4a0d22322963ee0c0426be3730
CountVa <pveryayev@bk.ru>
2022-12-21 08:53:12 +0300

    d

--- 12.py
+++ 12.py
@@ -3,12 +3,14 @@ def dfdf():
     write1 = open(file="12,1.txt", mode="w")
     with open(file="12.txt", mode="r") as f:
         for line in f:
-            if line.find('#') != 0:
+            if line.find('#') != -1:
                 i = line.find('#')
                 print(line[:i])
+                continue
             elif line.find('!') != 0:
                 j = line.find('!')
                 print(line[:j])
+                continue
             else:
                 print(line)
 
